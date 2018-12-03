d7e88ee97ee1bc245bb7fa0c1a5f852e3da59c91
Alex Ramos <alex@example.com>
2018-12-03 08:47:11 +0100

    Añadido Pizza a Routes
    Ya fui capaz de hacer que salga por pantalla la panta de pizza

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -3,6 +3,7 @@ import { Router } from "@reach/router";
 import StorePicker from "./StorePicker";
 import App from "./App";
 import NotFound from "./NotFound";
+import Pizza from "./Pizza";
 
 const Routes = () => (
   <Router>
@@ -10,6 +11,7 @@ const Routes = () => (
     <App path="/store/:storeId" />
     {/* TODO: Hook up the new Pizza component to render @ "/secret-dinner" */}
     {/* HINT: You need to write code 👇 and also 👆 */}
+    <Pizza path="/secret-dinner" />
     <NotFound default />
   </Router>
 );
